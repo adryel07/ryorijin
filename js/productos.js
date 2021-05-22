abc17cb40dcec032f9e39e00dd92d436681618e4
Martin C <caputtimartin12@gmail.com>
2021-05-22 02:29:32 -0300

    Agregue JS para los contenedores de productos
    Tendria que haber hecho una funcion pero no se me ocurrio bien como con jquery y te ibas a poner a mariconear si la hacia con javascript nativo jajaaj
    Funcar funca todo

--- js/productos.js
+++ js/productos.js
@@ -1,21 +1,9 @@
-$(document).ready(function(){
-	
-	document.getElementById("butIn").onclick = function (){
-
-	var carro =	document.querySelectorAll(".carrito");
-
-	for (var i = 0; i < carro.length ; i++) {
-		carro[i].style.display="initial";
-	}
-
-
-}
-});
 
 
 
 
 
+/*
 // Call carousel manually
 $('#myCarouselCustom').carousel();
 
@@ -42,10 +30,156 @@ wrap (booleano) – Especifica si el carrusel debe ciclar continuamente o detern
 keyboard (booleano) – Especifica si el carrusel debe reaccionar a los eventos del teclado. El valor por defecto es true.
 
 El siguiente ejemplo muestra cómo definir las opciones del carrusel.*/
-
+/*
 
 $('.carousel').carousel({
      interval: 8000,
      pause:true,
      wrap:false
+});
+*/
+
+/* desplegables productos 
+
+*/
+
+//Tendria que armarlo en una funcion 
+/*
+function mostrar(propio ,otro , parte){
+	propio.hide();
+	otro.show();
+	parte.show();
+}
+
+
+/*$("#butPescadoFrescoOpen").click(function(){
+	mostrar($("#butPescadoFrescoOpen"),$("butPescadoFrescoClose"),$("#pescFrequi"));
+});*/
+
+//Pescado Fresco
+$("#butPescadoFrescoOpen").click(function(){
+	 $("#butPescadoFrescoOpen:visible").hide();
+	 $("#butPescadoFrescoClose:hidden").show();
+	 $("#pescFrequi").show();
+});
+
+$("#butPescadoFrescoClose").click(function(){
+	$("#butPescadoFrescoOpen:hidden").show();
+	$("#butPescadoFrescoClose:visible").hide();
+	$("#pescFrequi").hide();
+
+});
+
+//Pescado Congelado
+$("#butPescadoCongeladoOpen").click(function(){
+	 $("#butPescadoCongeladoOpen").hide();
+	 $("#butPescadoCongeladoClose").show();
+	 $("#pescCongelado").show();
+});
+
+$("#butPescadoCongeladoClose").click(function(){
+	$("#butPescadoCongeladoOpen").show();
+	$("#butPescadoCongeladoClose").hide();
+	$("#pescCongelado").hide();
+});
+
+//Pollo congelado 
+$("#butPolloCongeladoOpen").click(function(){
+	 $("#butPolloCongeladoOpen").hide();
+	 $("#butPolloCongeladoClose").show();
+	 $("#polloCongelado").show();
+});
+
+$("#butPolloCongeladoClose").click(function(){
+	$("#butPolloCongeladoOpen").show();
+	$("#butPolloCongeladoClose").hide();
+	$("#polloCongelado").hide();
+});
+
+//Mariscos 
+$("#butMariscosOpen").click(function(){
+	 $("#butMariscosOpen").hide();
+	 $("#butMariscosClose").show();
+	 $("#mariscos").show();
+});
+
+$("#butMariscosClose").click(function(){
+	$("#butMariscosOpen").show();
+	$("#butMariscosClose").hide();
+	$("#mariscos").hide();
+});
+
+//Especiales
+
+
+$("#butEspecialesOpen").click(function(){
+	 $("#butEspecialesOpen").hide();
+	 $("#butEspecialesClose").show();
+	 $("#especiales").show();
+});
+
+$("#butEspecialesClose").click(function(){
+	$("#butEspecialesOpen").show();
+	$("#butEspecialesClose").hide();
+	$("#especiales").hide();
+});
+
+//Picadas Congeladas 
+
+
+
+$("#butPicaCongeladasOpen").click(function(){
+	 $("#butPicaCongeladasOpen").hide();
+	 $("#butPicaCongeladasClose").show();
+	 $("#picaCongeladas").show();
+});
+
+$("#butPicaCongeladasClose").click(function(){
+	$("#butPicaCongeladasOpen").show();
+	$("#butPicaCongeladasClose").hide();
+	$("#picaCongeladas").hide();
+});
+
+//Union Ganadera 
+
+unGan
+
+$("#butUnGanOpen").click(function(){
+	 $("#butUnGanOpen").hide();
+	 $("#butUnGanClose").show();
+	 $("#unGan").show();
+});
+
+$("#butUnGanClose").click(function(){
+	$("#butUnGanOpen").show();
+	$("#butUnGanClose").hide();
+	$("#unGan").hide();
+});
+
+//Salsas 
+
+$("#butSalAdhOpen").click(function(){
+	 $("#butSalAdhOpen").hide();
+	 $("#butSalAdhClose").show();
+	 $("#salAdh").show();
+});
+
+$("#butSalAdhClose").click(function(){
+	$("#butSalAdhOpen").show();
+	$("#butSalAdhClose").hide();
+	$("#salAdh").hide();
+});
+
+//McCain
+
+  $("#butMcCainOpen").click(function(){
+	 $("#butMcCainOpen").hide();
+	 $("#butMcCainClose").show();
+	 $("#mcCainn").show();
+});
+
+$("#butMcCainClose").click(function(){
+	$("#butMcCainOpen").show();
+	$("#butMcCainClose").hide();
+	$("#mcCainn").hide();
 });
