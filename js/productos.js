7b1b06895033200d01d918d12a434da838f10d01
adryel07 <ari.ag.1993@gmail.com>
2021-05-25 15:13:57 -0300

    Se soluciono el problema de productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,24 +1,4 @@
 
-/*
-
-Opciones del carrusel
-
-Puedes configurar las siguientes opciones en carrusel de Bootstrap según tus necesidades.
-
-interval (número) – Define la cantidad de tiempo entre cada uno de los elementos del slide. Puedes ponerlo a false para que el slider no se reproduzca automáticamente. El tiempo por defecto es de 5000 milisegundos.
-pause (cadena|null) – Pausa el ciclo cuando el ratón se pose encima, y lo reanuda cuando el ratón abandona el slide. Puedes definir este valor a false para impedir este comportamiento. El valor por defecto de esta opción es hover.
-wrap (booleano) – Especifica si el carrusel debe ciclar continuamente o deternerse en el último elemento. El valor por defecto es true.
-keyboard (booleano) – Especifica si el carrusel debe reaccionar a los eventos del teclado. El valor por defecto es true.
-
-El siguiente ejemplo muestra cómo definir las opciones del carrusel.*/
-/*
-
-$('.carousel').carousel({
-     interval: 8000,
-     pause:true,
-     wrap:false
-});
-*/
 
 /* desplegables productos 
 
@@ -36,25 +16,6 @@ function mostrar(propio ,otro , parte){
 /*$("#butPescadoFrescoOpen").click(function(){
 	mostrar($("#butPescadoFrescoOpen"),$("butPescadoFrescoClose"),$("#pescFrequi"));
 });*/
-
-
-
-$(document).ready(function(){
-
-
-// Call carousel manually
-$('#myCarouselCustom').carousel();
-
-// Go to the previous item
-$(".left").click(function(){
-    $("#myCarouselCustom").carousel("prev");
-});
-// Go to the previous item
-$(".right").click(function(){
-    $("#myCarouselCustom").carousel("next");
-});
-
-
 
 
 
@@ -193,5 +154,3 @@ $("#butPrincipio").click(function(){
 	document.documentElement.scrollTop = 0;
 });
 
-
-});
